fa62fbcc9b3180fcb68d69a2a2ebb2f48b2da4b7
manvigoyal20 <manvigoyal123@outlook.com>
2023-09-27 17:31:51 +0530

    asd

---
+++ src/test/java/com/example/springboot/CalculatorTest.java
@@ -0,0 +1,25 @@
+package com.example.springboot;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+    @Test
+    public void evaluatesExpression() {
+       Calculator calculator = new Calculator();
+       int sum = calculator.evaluate("1+2+3");
+       assertEquals(6, sum);
+    }
+    
+}
+// package com.javadevelopersguide.junit;
+// import static org.junit.Assert.assertEquals;
+// import org.junit.Test;
+
+// public class CalculatorTest {
+//      @Test
+//      public void evaluatesExpression() {
+//         Calculator calculator = new Calculator();
+//         int sum = calculator.evaluate("1+2+3");
+//         assertEquals(6, sum);
+//      }
+// }
