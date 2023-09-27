7e6f6753eb83356680004ed7be67ce09ae5d3a5d
manvigoyal20 <manvigoyal123@outlook.com>
2023-09-27 17:16:21 +0530

    change

--- src/test/java/com/example/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/example/springboot/SpringbootApplicationTests.java
@@ -7,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SpringbootApplicationTests {
 	@Test
 	void contextLoads() {
-		assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));  
-        assertEquals(-1,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));
 	}
 
 }
