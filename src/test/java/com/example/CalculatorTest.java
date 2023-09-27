75a6fc5172990f73bb87fcfe471b99bd88547a7a
manvigoyal20 <manvigoyal123@outlook.com>
2023-09-27 17:24:14 +0530

    test updated

---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.javadevelopersguide.junit;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+     @Test
+     public void evaluatesExpression() {
+        Calculator calculator = new Calculator();
+        int sum = calculator.evaluate("1+2+3");
+        assertEquals(6, sum);
+     }
+}
