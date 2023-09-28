35294c1f590192a0588ed2d53dbdb4c00b099994
manvigoyal20 <manvigoyal123@outlook.com>
2023-09-28 12:45:55 +0530

    c

--- src/test/java/com/example/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/example/springboot/SpringbootApplicationTests.java
@@ -1,26 +1,31 @@
+// package com.example.springboot;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class SpringbootApplicationTests {
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
+
 package com.example.springboot;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringbootApplicationTests {
+public class SpringbootApplicationTests {
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
-// package com.javatpoint.testcase;  
-  
-// import static org.junit.Assert.*;  
-// import com.javatpoint.logic.*;  
-// import org.junit.Test;  
-  
-// public class TestLogic {  
-  
-//     @Test  
-//     public void testFindMax(){  
-//         assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));  
-//         assertEquals(-1,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));  
-//     }  
-// }  
+
+
