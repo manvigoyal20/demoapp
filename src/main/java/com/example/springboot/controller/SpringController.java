e0c315071937706a15ee630125546999cee48ff7
manvigoyal20 <manvigoyal123@outlook.com>
2023-09-28 12:45:36 +0530

    add

--- src/main/java/com/example/springboot/controller/SpringController.java
+++ src/main/java/com/example/springboot/controller/SpringController.java
@@ -1,29 +1,54 @@
+// package com.example.springboot.controller;
+
+// //import java.time.LocalDateTime;
+
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// @RestController
+// public class SpringController {
+
+// 	private transient Integer val = 0;
+	
+// 	@GetMapping(value = "/")
+// 	public String getValue() {
+
+// //		LocalDateTime date = LocalDateTime.now();
+// //
+// //		Integer year = date.getYear();
+// //		Integer month = date.getMonthValue();
+// //		Integer day = date.getDayOfMonth();
+// //		Integer hour = date.getHour();
+// //		Integer min = date.getMinute();
+// //		Integer sec = date.getSecond();
+// 		val++;
+// //		Integer count = year + month + day + hour + min + sec + 1;
+// 		String result = "Mr. DevOps" + val;
+// 		return result;
+// 	}
+// }
+
 package com.example.springboot.controller;
 
-import java.time.LocalDateTime;
-
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springboot.Employee;
 
 @RestController
 public class SpringController {
 
-	private transient Integer val = 0;
-	
-	@GetMapping(value = "/")
-	public String getValue() {
+	@RequestMapping(value = "/employee", method = RequestMethod.GET)
+	public Employee firstPage() {
 
-//		LocalDateTime date = LocalDateTime.now();
-//
-//		Integer year = date.getYear();
-//		Integer month = date.getMonthValue();
-//		Integer day = date.getDayOfMonth();
-//		Integer hour = date.getHour();
-//		Integer min = date.getMinute();
-//		Integer sec = date.getSecond();
-		val++;
-//		Integer count = year + month + day + hour + min + sec + 1;
-		String result = "Mr. DevOps" + val;
-		return result;
+		Employee emp = new Employee();
+		emp.setName("emp1");
+		emp.setDesignation("manager");
+		emp.setEmpId("1");
+		emp.setSalary(3000);
+
+		return emp;
 	}
+
 }
